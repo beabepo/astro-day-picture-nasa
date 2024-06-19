c02c2409b56d20696e099d42a780c1db78e0e472
beabepo <>
2024-06-19 16:34:41 +0100

    first commit

---
+++ src/components/PictureData.vue
@@ -0,0 +1,40 @@
+<template>
+
+    <body>
+        <div class="picture-search">
+            <h1> Welcome weary traveller and behold, as teh marvels unfold </h1>
+            <div class="toggleToSearch">
+                <button type="button" @click="getPictureData">Fetch me a picture of the day!</button>
+            </div>
+
+            <div v-if="state.loaded">
+            </div>
+            <div v-else>No picture here, Oh no 😢 </div>
+
+        </div>
+    </body>
+
+
+</template>
+<script>
+import axios from "axios"
+import { ref, reactive } from 'vue'
+
+const getPicture = ref('')
+
+export default {
+    components: {},
+    setup() {
+
+        const state = reactive({
+            loaded: false,
+            pictureData: '',
+        })
+
+        return {
+            state
+        }
+
+    }
+}
+</script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<script>
+import PictureData from '../components/PictureData.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    PictureData
+  }
+}
+</script>
+
+<template>
+  <main>
+    <PictureData />
+  </main>
+</template>
